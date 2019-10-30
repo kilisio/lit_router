d3d543304d8df6410604ff2829436f58c4312623
kilisio <pkilisio@gmail.com>
2019-10-30 18:14:23 +0300

    fix(views.js): imported lit-elements into views file

--- fork_src/views.js
+++ fork_src/views.js
@@ -1,3 +1,3 @@
-export { route_link } from './views/route_link';
-export { route_slot } from './views/route_slot';
-
+import './views/route_link';
+import './views/route_slot';
+import './views/demo_app.js';
