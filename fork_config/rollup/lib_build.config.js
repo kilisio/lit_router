d1f01fddfa43550bf7e016418fd8c0aac723459a
kilisio <pkilisio@gmail.com>
2019-10-30 18:56:29 +0300

    build(lib_build.config.js): added module library rollup build script

---
+++ fork_config/rollup/lib_build.config.js
@@ -0,0 +1,36 @@
+import clear from 'rollup-plugin-clear';
+import cleanup from 'rollup-plugin-cleanup';
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import * as pkgson from "./package.json";
+
+export default {
+    input: 'fork_src/lib.js',
+    output: {
+        file: 'fork_build/lib/lib.js',
+        format: 'esm'
+    },
+    plugins: [
+        clear({
+            targets: ['fork_build/lib'],
+        }),
+        resolve(),
+        commonJS({
+              include: 'node_modules/**'
+        }),
+        cleanup(),
+        terser(),
+        generatePackageJson({
+            outputFolder: 'fork_build/lib',
+            baseContents: {
+                "name": '@kilisio/' + pkgson.name + '_lib',
+                "version": pkgson.version,
+                "main": "lib.js",
+                "author": "kilisio ",
+                "license": "MIT"
+            }
+        })
+    ]
+};
