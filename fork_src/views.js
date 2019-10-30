e241c920012d6464a6c76747b3744fde0509c725
kilisio <pkilisio@gmail.com>
2019-10-30 17:59:43 +0300

    chore: file and folder reordering

---
+++ fork_src/views.js
@@ -0,0 +1,3 @@
+export { route_link } from './router/route_link';
+export { route_slot } from './router/route_slot';
+
--- views.js
+++
@@ -1,3 +0,0 @@
-export { route_link } from './router/route_link';
-export { route_slot } from './router/route_slot';
-
